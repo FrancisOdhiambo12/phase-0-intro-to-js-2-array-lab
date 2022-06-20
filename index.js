eb7d3333687a82b5043f65d4c073693fde683f57
Francis Odhiambo2 <francisodhiambo2@gmail.com>
2022-06-20 22:11:39 +0300

    commit tree 863326cacb1d35b2ed2b20eb713ba52976f7bbb0

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+   let newCat = cats.push('Ralph')
+   return newCat
+  };
+  destructivelyAppendCat(newCat)
+
+
+//   let cats = ["Milo", "Otis", "Garfield", "Ralph"]
+
+  function destructivelyPrependCat() {
+    let latestCat = cats.unshift('Bob')
+    return latestCat
+  }
+  destructivelyPrependCat(latestCat)
+
+  function destructivelyRemoveLastCat() {
+    let removeLastItem = cats.pop()
+    return removeLastItem
+    
+  }
+  destructivelyRemoveLastCat(removeLastItem)
+
+  function destructivelyRemoveFirstCat() {
+    let removeFirstCat = cats.shift()
+    return removeFirstCat
+  }
+  destructivelyRemoveFirstCat(removeFirstCat)
+  
+
+  function appendCat(name) {
+    let CatsArr = [...cats, "Broom"];
+    return CatsArr;
+}
+
+function prependCat(name) {
+    let CatsArray = ["Arnold", ...cats];
+    return CatsArray;
+}
+
+function appendCat(name) {
+    let catsArr = [...cats, "Broom"];
+    return catsArr;
+}
+appendCat(catsArr)
+
+function removeLastCat() {
+    let catListArr = cats.slice(0, -1);
+    return catListArr;
+}
+removeLastCat(catListArr)
+
+function removeFirstCat() {
+    let ListCats = cats.slice(-2);
+    return ListCats;
+}
+removeFirstCat(ListCats)
+
+  
